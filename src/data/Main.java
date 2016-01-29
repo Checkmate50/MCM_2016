ec8d7e1cf23cd9454e9c8c9ee2ab3a73c4483281
Checkmate50 <dgeisler50@gmail.com>
2016-01-28 23:11:24 -0700

    part-way to reading in data properly

--- src/data/Main.java
+++ src/data/Main.java
@@ -1,14 +1,16 @@
 package data;
 
+import java.io.File;
 import java.util.ArrayList;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		ArrayList<University> data;
-		
-		
+		File pos = new File("data/IPEDSUID.txt");
+		File dat = new File("data/CohortsData.txt");
+		File dic = new File("data/ScorecardDictionary.txt");
+		DataHolder data = new DataHolder(pos, dat, dic);
 	}
 
 }
--- src/data/University.java
+++ src/data/University.java
@@ -1,30 +1,32 @@
 package data;
 
+import java.util.Hashtable;
+
 public class University implements Comparable<University> {
 	private int unitid;
-	private String[] data;
-	public University(int unitid, String[] data) {
-		this.unitid = unitid;
-		this.data = data;
-	}
-	
-	public String[] getData() {
-		return this.data;
+	private Hashtable<String, String> fields;
+
+	public University(String[] titles, String[] data) {
+		//titles.length must equal data.length
+		for(int i = 0; i < data.length; i++) {
+			fields.put(titles[i], data[i]);
+		}
+		this.unitid = Integer.parseInt(fields.get("unitid"));
 	}
 	
 	public int getUnitid() {
 		return this.unitid;
 	}
 
+	public String getField(String field) {
+		return fields.get(field);
+	}
+	
 	public int compareTo(University arg0) {
 		return this.unitid - arg0.getUnitid();
 	}
 	
 	public String toString() {
-		String to_return = ((Integer)this.unitid).toString();
-		for(int i = 0; i < data.length; i++) {
-			to_return += data[i];
-		}
-		return  to_return;
+		return fields.get("name");
 	}
 }
